e078cbd443a0f5e62033c88033d612b293e94a3e
ahmedessam3le <ahmedessam3le@gmail.com>
2022-07-05 03:46:39 +0200

    Person Details & Image Download Features Added

--- lib/business_logic/cubits/person_images_cubit.dart
+++ lib/business_logic/cubits/person_images_cubit.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:tmdb/core/errors/failure.dart';
-import 'package:tmdb/core/utils/app_constanttrings.dart';
+import 'package:tmdb/core/utils/app_constants.dart';
 import 'package:tmdb/data/models/images_model.dart';
 import 'package:tmdb/data/repositories/person_images_repository.dart';
 
